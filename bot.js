e5e0c2c43106f03153229be7a91995733923ee02
unknown <forgoroe@gmail.com>
2017-09-04 20:14:54 +0200

    mess around with javascript promises

--- bot.js
+++ bot.js
@@ -1,43 +1,114 @@
 var Twit = require('twit');
 var config = require('./config');
-var actions = require('./scripts/actions');
-var MarkovChain = require('markovchain');
 
-var fs = require('fs');
-var data = readFile('./dataSets/eloquence.txt', 'utf8')
+console.log('beep boop', '\n');
 
-console.log('beep boop');
+var T = new Twit(config.keys);
 
-function readFile(path, encoding){
-	var data = fs.readFileSync(path, encoding);
-	return data;
+/*
+When one of my followers tweet something, check the contents of their tweets. If those tweets
+contain at least one of a particular keyword, post a tweet modifying those keywords with my own.
+*/
+var keywords = /witnesses|allegedly|new study|rebuild|space|google glass|smartphone|electric|senator|\bcar\b|election|congressional leaders|homeland security|could not be reached for comment/gi;
+
+
+// setInterval(run, 1000*60*15);
+run();
+
+function run(){
+	retrieveTweets()
+		.then(modifyTweets)
+		.catch((err)=>
+			console.log(err))
 }
 
-function generateMarkov(data){
+function retrieveTweets(){
+	return T.get('friends/list')
+		.then(checkTweets)
+		.catch(()=> {console.log("Couldn't retrieve tweets")}); //T.get (twit API) doesn't throw exception and returns an undefined object, which is why I catch error on undefined
+}
 
-	var useUpperCase = function(wordList) {
-	  var tmpList = Object.keys(wordList).filter(function(word) {
-	    return word[0] >= 'A' && word[0] <= 'Z'
-	  })
-	  return tmpList[~~(Math.random()*tmpList.length)]
+function postToTwitter(newTweetsParam){
+	statusObject = {
+		status: message,
+	}
+	
+	return T.post('statuses/update', statusObject).catch((err) => console.log(err));	
+}
+
+function checkTweets(result){
+	let author = result.data.users;
+	var toModify = [];
+
+	for (var i = 0; i < result.data.users.length; i++) {
+		let author = result.data.users[i].screen_name;
+		let originalTweet = result.data.users[i].status.text;
+
+		console.log(result.data.users[i].screen_name, result.data.users[i].status.text, '\n\n');
+
+		if(result.data.users[i].status){
+			let needsModifying = checkForTriggers(originalTweet);
+			
+			if(needsModifying){
+				toModify.push({'author': author, 'tweet': originalTweet});
+			}
+		}
 	}
 
-	return new MarkovChain(data).start('a').end(140).process();
+	let promise = new Promise(function(resolve, reject){
+		if(toModify.length){
+			resolve(toModify);
+		} else {
+			let currentdate = new Date(); 
+			let datetime = "Last Sync: "
+	        + currentdate.getHours() + ":"  
+	        + currentdate.getMinutes() + ":" 
+	        + currentdate.getSeconds();
+
+	        console.log('No tweets to modify');
+		}
+	});
+	return promise;
 }
 
-console.log(generateMarkov(data));
-var resultPlus = generateMarkov();
-
-/*var T = new Twit(config.keys);
-
-actions.tweet(T, {status: 'Testing again.'});
-
-//search params
-var params = {
-	q: 'sonic mania',
-	count: 1,
+function checkForTriggers(originalTweet){
+	if(originalTweet.search(keywords)!==-1){
+		return true
+	}
+	return false;
 }
 
-actions.search(T, params);
 
-*/
+function modifyTweets(stuffToModify){
+	let newTweets = [];
+	var mapObj = {
+   		witnesses: "these dudes I know",
+   		allegedly: "kinda probably",
+   		'new study': "tumblr post",
+   		rebuild: "avenge",
+   		space: "spaaace",
+   		'google glass': "virtual boy",
+   		smartphone: "pokedex", 
+   		electric: "atomic",
+   		senator: "elf-lord",
+   		car: "cat",
+   		election: "eating contest",
+   		'congressional leaders': "river spirits",
+   		'homeland security': "homestar runner", 
+   		'could not be reached for comment': "is guilty and everyone knows it"
+	};
+
+	for(var i = 0; i<stuffToModify.length; i++){
+			var modifiedTweet =	stuffToModify[i].tweet.replace(keywords, function(matched){
+			return mapObj[matched];
+		});
+		console.log("modified tweet: ", modifiedTweet);
+		
+		newTweets.push({
+			author: stuffToModify[i].author,
+			tweet: modifiedTweet
+		});
+	}
+
+	return newTweets;
+}
