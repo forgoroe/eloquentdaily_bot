f388b542fa64609869bb5b351a9c3e56f8d79645
unknown <forgoroe@gmail.com>
2017-09-05 18:31:50 +0200

    changes for heroku branch exclusively

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports.keys = {
+	consumer_key:         'iZe5VUzBIbnCaoaus1lq7f2Hi',
+ 	consumer_secret:      'AMcIX7v2xlhgbJDBgwS2kU9uFEEJPPVoUQev8NiFzwShcmXr9L',
+  	access_token:         '903337579831025665-yXzQeEpPSYXJHb8Q8ZjKzFTyCIMjvDH',
+  	access_token_secret:  '3BYkJxPEtpNwgkswEJSumDVeR9ZAfgL2x161mASvp3GpW',
+}
